7a8b5482c3eff68b74b10be3c0fd19def0c04d0b
VeberAL <aleo83@rambler.ru>
2021-10-25 22:13:14 +0300

    Лимитированное количество элементов при помощи stream

---
+++ src/LimitStream.java
@@ -0,0 +1,11 @@
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class LimitStream {
+    public static void main(String[] args) {
+        //Лимитированное количество элементов
+        List arr = Stream.of(1,2,3,4,5,6,7,8,9,10).limit(5).collect(Collectors.toList());
+        System.out.println(arr);
+    }
+}
--- src/MinMaxStream.java
+++ src/MinMaxStream.java
@@ -3,8 +3,9 @@ import java.util.stream.Stream;
 
 public class MinMaxStream {
     public static void main(String[] args) {
-    int value1 = Stream.of(4,-6,1,2,4,17).min(Comparator.comparing(x -> x)).get();
-    int value2 = Stream.of(1,2,-4,17,5).max(Comparator.comparing(x -> x)).get();
-        System.out.println(value1 +" "+ value2);
+        //Поиск минимального и максимального значения в коллекции
+        int value1 = Stream.of(4,-6,1,2,4,17).min(Comparator.comparing(x -> x)).get();
+        int value2 = Stream.of(1,2,-4,17,5).max(Comparator.comparing(x -> x)).get();
+        System.out.println("Минимальный элемент:"+ value1 +". Максимальный элемент: "+ value2);
     }
 }
